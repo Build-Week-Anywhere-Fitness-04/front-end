96cf6a029c56e83d5c70646ad45fe6a2184f2c42
Sone <sthaya@gmail.com>
2020-04-25 20:36:54 -0700

    created navbar component and added links to login and signup page. added a script tag to index.html for the captcha in the signup component

--- client/src/components/MainApp.js
+++ client/src/components/MainApp.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route } from "react-router-dom";
 import SignUp from "./SignUp";
 import Login from "./Login";
+import Navbar from './Navbar'
 
 const MainApp = () => {
   return (
     <div>
+      <Navbar />
       
       <Route exact path="/signup">
         <SignUp />
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <header>
+          
+        <h1>AnyWhere Fitness</h1>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/login'>Login</Link>
+          <Link to='/signup'>Sign Up</Link>
+        </div>
+    
+      </header>
+      
+    </div>
+  )
+}
+
+export default Navbar
