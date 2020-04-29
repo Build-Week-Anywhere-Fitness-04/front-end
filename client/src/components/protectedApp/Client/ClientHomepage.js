d243a77e0859fb2c3495f3d34fa0a16f97e9cf37
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-28 21:09:47 -0700

    client now able to see all classes avalble

--- client/src/components/protectedApp/Client/ClientHomepage.js
+++ client/src/components/protectedApp/Client/ClientHomepage.js
@@ -1,19 +1,35 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { axiosWithAuth } from "../../../utils/axiosWithAuth";
 import Navbar from "./Navbar";
 import Header from "./Header";
 import ClientDisplayClasses from "./ClientDisplayClasses";
 
 const ClientHomePage = () => {
+  const dispatch = useDispatch();
   const reducer = useSelector((state) => ({
     ...state,
   }));
+  const { allClasses } = reducer.clientReducer;
   //   console.log("reducer here", reducer.clientReducer);
+  useEffect(() => {
+    dispatch({ type: "FETCHING_CLIENT_CLASSES" });
+    axiosWithAuth()
+      .get(`/api/classes`)
+      .then((res) => {
+        //   console.log(res);
+        dispatch({ type: "SAVING_CLIENT_CLASSES", payload: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: "SAVING_ERROR", payload: err });
+      });
+  }, []);
   return (
     <div>
       <Navbar />
       <Header />
-      <ClientDisplayClasses />
+      <ClientDisplayClasses allClasses={allClasses} />
     </div>
   );
 };
